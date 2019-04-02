9f1dad9bcf93d79780990cdbb62481c9d738c242
acesur <sures.chaudhary0@gmail.com>
2019-04-03 05:22:29 +0545

    Testing Push github

---
+++ AttendThat/Attendance/Attendance/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Attendance.Models;
+using System.Data.SqlClient;
+
+namespace Attendance.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void connectionString()
+        {
+            con.ConnectionString = "data source =DESKTOP-1GI0ER8\\SQLSERVER; database=Login; integrated security = true;";
+        }
+        [HttpPost]
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from tbl_login where username = '"+acc.Name+"' and password = '"+acc.Password+"'";
+            dr = com.ExecuteReader();
+            if(dr.Read())
+            {
+                con.Close();
+               
+                return Redirect("~/WebFormAttend.aspx");
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+            
+            
+        }
+    }
+}

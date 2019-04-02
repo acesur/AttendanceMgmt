69d51eca294dc856dd182b0c96469bd932a838ed
acesur <sures.chaudhary0@gmail.com>
2019-04-03 05:32:49 +0545

    Test cases Activity Diagram

---
+++ Implementation/Sql data/dbo.AttendStatus.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[AttendStatus]
+(
+	[RollId] INT NOT NULL , 
+    [StudentName] VARCHAR(50) NOT NULL, 
+    [DateOfClass] VARCHAR(50) NOT NULL, 
+    [AttendStatus] VARCHAR(50) NOT NULL, 
+    [Class] VARCHAR(50) NOT NULL
+)
---
+++ Implementation/Sql data/dbo.StudentDetails.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[StudentDetails]
+(
+	[RollID] INT NOT NULL, 
+    [StudentName] VARCHAR(50) NOT NULL, 
+    [Class] VARCHAR(50) NOT NULL 
+)
